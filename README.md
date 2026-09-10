d2b7f0657c20df6ce91d708094ef810921ea3c79
Hursh Patel <hurshb50@gmail.com>
2026-09-10 18:17:22 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# setup-formatter
+
+CLI to handle installing, configuring, and adding scripts for oxfmt
---
+++ vitest.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from "vitest/config"
+
+const configuration = defineConfig({
+
+})
+
+export default configuration
